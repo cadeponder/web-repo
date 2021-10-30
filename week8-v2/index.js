5e303f9c51eb962b5fc47b17cb1734c7f1b672d5
cadeponder <89998363+cadeponder@users.noreply.github.com>
2021-10-29 20:58:03 -0600

    s

--- week8-v2/index.js
+++ week8-v2/index.js
@@ -4,5 +4,5 @@ days=["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"];
 for (i=0; i<document.getElementByClassName("days").length; i++){
   document.getElementsByClassName("days")[i].innerHTML= "<h2>"+days[i]+"</h2>";
   document.getElementsByClassName("days")[i].addEventListener('click',setActive,false);
-  document.getElementsByClassName("days")[i].id= days[i];
+  document.getElementsByClassName("days")[i].id = days[i];
 }
